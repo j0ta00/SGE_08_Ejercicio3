eb92df599d178cfbb9c059215ba9f2c2816c06e9
j0ta00 <rypperx@gmail.com>
2021-11-04 21:23:59 +0100

    Agregar archivos de proyecto.

---
+++ SGE_08_Ejercicio3/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MisClases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGE_08_Ejercicio3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Editar()
+        {
+            clsPersona persona = new clsPersona("juanyo","android >:I");
+            return View(persona);
+        }
+        [HttpPost]
+        public IActionResult Editar(clsPersona clsPersona)
+        {
+            return View("PersonaModificada",clsPersona);
+        }
+    }
+}
---
+++ SGE_08_Ejercicio3/models/clsPersona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MisClases
+{
+    public class clsPersona
+    {
+
+        #region atributos privados
+        private String name;
+        private String surname;
+       
+
+
+        #endregion
+
+
+        //Forma 1
+        public string Name { get => name; set => name = value; }
+        public string Surname { get => surname; set =>surname=value; }
+
+        public clsPersona()
+        {
+            this.name = "";
+        }
+        public clsPersona(string name,string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+    }
+}
